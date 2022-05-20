4ce0f088211d499cc5f65a39c16dd285aea504c2
mxochicale <perez.xochicale@gmail.com>
2022-05-20 07:42:41 +0100

    adds skills & comments for 2-page template CICV2P (#15)

--- docs/pdf2png.bash
+++ docs/pdf2png.bash
@@ -7,10 +7,9 @@
 
 
 wget https://github.com/mxochicale/cv/raw/generated-pdfs/cv-two-pages.pdf
-#https://github.com/mxochicale/cv/blob/generated-pdfs/cv-two-pages.pdf
 mv cv-two-pages.pdf input.pdf
-convert -background white -alpha remove -alpha off -density 150 input.pdf -quality 90 output.png
+convert -background white -alpha remove -alpha off -density 500 input.pdf -quality 300 output.png
+rm *.pdf
 #ALTERNATIVES
 #convert cv-two-pages.pdf cv-two-pages.png
 #pdftoppm -png input.pdf > myfile.png
-rm *.pdf
