b1047beeab6eee478af77bc49dfd42ae7050dc3e
Miguel Xochicale, PhD <perez.xochicale@gmail.com>
2025-09-01 20:42:07 +0100

    44 - sep2025 (#45)
    * adds john-nocum project and benjamin duvieusart paper #44
    * :tada: cv-1-sep-2025 has been updated > adds bit for both all publications and latest-ten-publications; stylistics changes to CV (margins, sections distances); update main short intro in the abstract; updaets workflow to accept any branch CICV2P #44
    * updates pdf2png.bash using ci pdf; renaming files and updating main README; solves #44
    * minor changes

--- docs/pdf2png.bash
+++ docs/pdf2png.bash
@@ -6,10 +6,12 @@
 # https://superuser.com/questions/185880/how-to-convert-a-pdf-document-to-png
 
 sudo apt-get install -y imagemagick
-cp ../tex/cv-two-pages.pdf .
 
-mv cv-two-pages.pdf input.pdf
+#download ci-version
+wget https://raw.githubusercontent.com/mxochicale/cv/refs/heads/generated-pdfs/cv-two-pages.pdf
+
+#mv cv-two-pages.pdf input.pdf
 DENSITY=100 #500
 QUALITY=500 #300
-convert -background white -alpha remove -alpha off -density ${DENSITY} input.pdf -quality ${QUALITY} page.png
+convert -background white -alpha remove -alpha off -density ${DENSITY} cv-two-pages.pdf -quality ${QUALITY} cv-page.png
 rm *.pdf
