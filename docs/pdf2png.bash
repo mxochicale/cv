adb2e4fc7f2be923b2a84a749a0b2d9cf9d93f22
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2026-04-06 17:43:18 +0100

    adds png pages, a few changes to pdf2png bash and main readme #48

--- docs/pdf2png.bash
+++ docs/pdf2png.bash
@@ -5,10 +5,10 @@
 # References 
 # https://superuser.com/questions/185880/how-to-convert-a-pdf-document-to-png
 
-sudo apt-get install -y imagemagick
+# sudo apt-get install -y imagemagick
 
 #download ci-version
-wget https://raw.githubusercontent.com/mxochicale/cv/refs/heads/generated-pdfs/cv-two-pages.pdf
+wget -4 https://raw.githubusercontent.com/mxochicale/cv/refs/heads/generated-pdfs/cv-two-pages.pdf
 
 #mv cv-two-pages.pdf input.pdf
 DENSITY=100 #500
